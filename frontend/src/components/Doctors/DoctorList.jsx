b1b357c42c886358a795a430e74a7decbbcd9157
mwangea <mwangeamusa@gmail.com>
2023-07-30 10:39:46 +0300

    Initial commit

---
+++ frontend/src/components/Doctors/DoctorList.jsx
@@ -0,0 +1,12 @@
+import DoctorCard from './DoctorCard';
+import { doctors } from './../../assets/data/doctors';
+
+const DoctorList = () => {
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]'>
+    {doctors.map((doctor) => <DoctorCard doctor={doctor}  key={doctor.id}/>)}
+   </div>
+  )
+}
+
+export default DoctorList
