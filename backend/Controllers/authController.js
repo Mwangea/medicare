76b44353a913c3ea60e1b0a25b612b5b1b0909db
mwangea <mwangeamusa@gmail.com>
2023-09-10 00:10:48 +0300

    started authentication part in backend

---
+++ backend/Controllers/authController.js
@@ -0,0 +1,21 @@
+import User from "../models/UserSchema.js"
+
+export const register = async(req,res) =>{
+
+    const{email, password, name, role, photo, gender} = req.body
+    try {
+        
+        let user = null
+
+    } catch (error) {
+        
+    }
+};
+
+export const login = async(req,res) =>{
+    try {
+        
+    } catch (error) {
+        
+    }
+};
