97c7a9b28cb75ab5a95aaee820e0b5d9652c3337
mwangea <mwangeamusa@gmail.com>
2023-10-21 13:19:08 +0300

    fixed some few errors

--- frontend/src/Dashboard/user-account/MyAccount.jsx
+++ frontend/src/Dashboard/user-account/MyAccount.jsx
@@ -1,6 +1,6 @@
 import { useContext, useState } from 'react';
 
-import userImg from "../../assets/images/doctor-img01.png";
+
 import { AuthContext } from '../../context/AuthContext';
 
 import MyBookings from './MyBookings';
@@ -20,7 +20,7 @@ const MyAccount = () => {
     
     const {data:userData, loading, error} = useGetProfile(`${BASE_URL}/users/profile/me`)
 
-    console.log(userData, 'userdata');
+    //console.log(userData, 'userdata');
     const handleLogout = () => {
         dispatch({type: 'LOGOUT'});
     }
@@ -37,14 +37,14 @@ const MyAccount = () => {
             <div className='pb-[50px] px-[30px] rounded-md'>
                 <div className='flex items-center justify-center'>
                     <figure className='w-[100px] h-[100px] rounded-full border-2 border-solid border-primaryColor'>
-                        <img src={userImg} alt='' className='w-full h-full rounded-full'/>
+                        <img src={userData.photo} alt='' className='w-full h-full rounded-full'/>
                     </figure>
                 </div>
                 <div className='text-center mt-4'>
-                    <h3 className='text-[18px] leading-[30px] text-headingColor font-bold'>Musa Mwangea</h3>
-                    <p className='text-textColor text-[15px] leading-6 font-medium'>example@gmail.com</p>
+                    <h3 className='text-[18px] leading-[30px] text-headingColor font-bold'>{userData.name}</h3>
+                    <p className='text-textColor text-[15px] leading-6 font-medium'>{userData.email}</p>
                     <p className='text-textColor text-[15px] leading-6 font-medium'>
-                        Blood Type: <span className='ml-2 text-headingColor text-[22px] leading-8'>o-</span>
+                        Blood Type: <span className='ml-2 text-headingColor text-[22px] leading-8'>{userData.bloodType}</span>
                     </p>
                 </div>
                 <div className='mt-[50px] md:mt-[100px]'>
@@ -75,7 +75,7 @@ const MyAccount = () => {
                     tab === 'bookings' && <MyBookings />
                 },
                 {
-                    tab === 'settings' && <Profile />
+                    tab === 'settings' && <Profile user={userData}/>
                 }
             </div>
         </div>
--- frontend/src/Dashboard/user-account/Profile.jsx
+++ frontend/src/Dashboard/user-account/Profile.jsx
@@ -1,180 +1,182 @@
-
-
-import {  useNavigate } from 'react-router-dom';
-import { useState } from 'react';
+/* eslint-disable react/prop-types */
+import  { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import uploadImageToCloudinary from '../../utils/uploadCloudinary';
-import { BASE_URL } from '../../config';
-import {toast} from 'react-toastify'
+import { BASE_URL, token } from '../../config';
+import { toast } from 'react-toastify';
 import HashLoader from 'react-spinners/HashLoader';
 
-const Profile = () => {
-
-  const [selectedFile, setSelectedFile] = useState(null);
-  const [previewURL, setPreviewURL] = useState('');
-  const [loading, setLoading] = useState(false);
-
-  const [formData, setFormData] = useState({
-    name:'',
-    email:'',
-    password:'',
-    photo:'',
-    gender:'',
-    role:'patient'
-  });
-
+const Profile = ({ user }) => {
   const navigate = useNavigate();
 
-  const handleInputChange = e => {
-    setFormData({ ...formData, [e.target.name]: e.target.value })
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    photo: 'null',
+    gender: '',
+    bloodType: '',
+  });
+
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setFormData({
+      name: user.name,
+      email: user.email,
+      photo: user.photo,
+      gender: user.gender,
+      bloodType: user.bloodType,
+    });
+  }, [user]);
+
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleFileInputChange = async (event) =>{
-
-    const file = event.target.files[0]
-
+  const handleFileInputChange = async (event) => {
+    const file = event.target.files[0];
     const data = await uploadImageToCloudinary(file);
+    setSelectedFile(data.url);
+    setFormData({ ...formData, photo: data.url });
+  };
 
-    setPreviewURL(data.url) 
-    setSelectedFile(data.url)
-    setFormData({...formData, photo:data.url })
-  // later we will use cloudinary to upload images
-    {/* console.log(file) */}
-  }
-
-  const submitHandler = async event=>{
-
-    
+  const submitHandler = async (event) => {
     event.preventDefault();
-    setLoading(true)
+    setLoading(true);
 
     try {
-      const res = await fetch(`${BASE_URL}/auth/register`, {
-        method: 'post',
+      const res = await fetch(`${BASE_URL}/users/${user._id}`, {
+        method: 'put',
         headers: {
-          'content-Type': 'application/json'
+          'content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
         },
-        body: JSON.stringify(formData)
+        body: JSON.stringify(formData),
       });
 
-      const {message} = await res.json()
+      const { message } = await res.json();
 
-      if(!res.ok){
-        throw new Error(message)
+      if (!res.ok) {
+        throw new Error(message);
       }
 
       setLoading(false);
       toast.success(message);
-      navigate('/login')
+      navigate('/users/profile/me');
     } catch (err) {
-      toast.error(err.message)
-      setLoading(false)
+      toast.error(err.message);
+      setLoading(false);
     }
+  };
 
-  };  
   return (
-    <div>
+    <div className="mt-10">
       <form onSubmit={submitHandler}>
-          <div className="mb-1"> {/* Adjusted margin here */}
-            <input type="text"
-              placeholder="Full Name"
-              name="name"
-              value={formData.name}
-              onChange={handleInputChange}
-              className="w-full py-2 border-b border-solid border-[#0066ff61]
+        <div className="mb-1">
+          <input
+            type="text"
+            placeholder="Full Name"
+            name="name"
+            value={formData.name}
+            onChange={handleInputChange}
+            className="w-full py-2 border-b border-solid border-[#0066ff61]
                 focus:outline-none focus:border-b-primaryColor text-[16px] leading-6
                 text-headingColor placeholder:text-textColor cursor-pointer"
-              required
-            />
-          </div>
-          <div className="mb-1"> {/* Adjusted margin here */}
-            <input type="email"
-              placeholder="Enter your email"
-              name="email"
-              value={formData.email}
-              onChange={handleInputChange}
-              className="w-full py-2 border-b border-solid border-[#0066ff61]
+            required
+          />
+        </div>
+        <div className="mb-1">
+          <input
+            type="email"
+            placeholder="Enter your email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            className="w-full py-2 border-b border-solid border-[#0066ff61]
                 focus:outline-none focus:border-b-primaryColor text-[16px] leading-6
                 text-headingColor placeholder:text-textColor cursor-pointer"
-              required
-            />
-          </div>
-          <div className="mb-3"> {/* Adjusted margin here */}
-            <input type="password"
-              placeholder="Enter your password"
-              name="password"
-              value={formData.password}
-              onChange={handleInputChange}
-              className="w-full py-2 border-b border-solid border-[#0066ff61]
+            readOnly
+          />
+        </div>
+        <div className="mb-3">
+          <input
+            type="password"
+            placeholder="Enter your password"
+            name="password"
+            value={formData.password}
+            onChange={handleInputChange}
+            className="w-full py-2 border-b border-solid border-[#0066ff61]
                 focus:outline-none focus:border-b-primaryColor text-[16px] leading-6
                 text-headingColor placeholder:text-textColor cursor-pointer"
-              required
-            />
-          </div>
-          <div className="mb-5 flex items-center justify-between">
-            <label 
-            className='text-headingColor font-bold text-[15px] leading-7 px-4 py-3 focus:outline-none'
+          />
+        </div>
+        <div className="mb-3">
+          <input
+            type="text"
+            placeholder="Blood Type"
+            name="bloodType"
+            value={formData.bloodType}
+            onChange={handleInputChange}
+            className="w-full py-2 border-b border-solid border-[#0066ff61]
+                focus:outline-none focus:border-b-primaryColor text-[16px] leading-6
+                text-headingColor placeholder:text-textColor cursor-pointer"
+            required
+          />
+        </div>
+        <div className="mb-5 flex items-center justify-between">
+          <label className="text-headingColor font-bold text-[15px] leading-7 px-4 py-2 focus:outline-none">
+            Gender:
+            <select
+              name="gender"
+              value={formData.gender}
+              onChange={handleInputChange}
+              className="text-textColor font-semibold text-[15px] leading-7 px-4 py-3 focus:outline-none"
             >
-             Are you a:
-             <select 
-             name='role'
-             value={formData.role}
-             onChange={handleInputChange}
-             className='text-textColor font-semibold text-[15px] leading-7 px-4 py-3 focus:outline-none'
-             >
-              <option value="patient">Patient</option>
-              <option value="doctor">Doctor</option>
-             </select>
-            </label>
-
-            <label 
-            className='text-headingColor font-bold text-[15px] leading-7 px-4 py-2 focus:outline-none'
-            >
-             Gender:
-             <select 
-             name='gender'
-             value={formData.gender}
-             onChange={handleInputChange}
-             className='text-textColor font-semibold text-[15px] leading-7 px-4 py-3 focus:outline-none'
-             >
               <option value="">Select</option>
               <option value="male">Male</option>
               <option value="female">Female</option>
               <option value="custom">Custom</option>
-
-             </select>
+            </select>
+          </label>
+        </div>
+        <div className="mb-3 flex items-center gap-3">
+          {formData.photo && (
+            <figure className="w-[60px] h-[60px] rounded-full border-2 border-solid border-primaryColor flex items-center justify-center">
+              <img src={formData.photo} alt="" className="w-full rounded-full" />
+            </figure>
+          )}
+          <div className="relative w-[160px] h-[50px]">
+            <input
+              type="file"
+              name="photo"
+              id="customFile"
+              onChange={handleFileInputChange}
+              accept=".jpg, .png"
+              className="absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer"
+            />
+            <label
+              htmlFor="customFile"
+              className="absolute top-0 left-0 w-full h-full flex items-center px-[0.75rem] py-[0.375rem] text-[15px] leading-6 overflow-hidden bg-[#0066ff45]
+               text-headingColor font-semibold rounded-lg truncate cursor-pointer"
+            >
+              {selectedFile ? selectedFile.name : 'Upload Photo'}
             </label>
           </div>
-          <div className='mb-3 flex items-center gap-3'>
-            { selectedFile && <figure className='w-[60px] h-[60px] rounded-full border-2 border-solid border-primaryColor flex items-center justify-center'>
-              <img src={previewURL} alt=''  className='w-full rounded-full'/>
-            </figure>}
-            <div className='relative w-[160px] h-[50px]'>
-              <input
-               type='file'
-               name='photo'
-               id='customFile'
-               onChange={handleFileInputChange}
-               accept='.jpg, .png'
-               className='absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer'
-               />
-               <label htmlFor='customFile' 
-               className='absolute top-0 left-0 w-full h-full flex items-center px-[0.75rem] py-[0.375rem] text-[15px] leading-6 overflow-hidden bg-[#0066ff45]
-               text-headingColor font-semibold rounded-lg truncate cursor-pointer'>
-                Upload Photo
-               </label>
-            </div>
-          </div>
-          <div className="mt-3"> {/* Adjusted margin here */}
-            <button
-            disabled={loading && true}
-              type="submit"
-              className="w-full bg-primaryColor text-white text-[18px] leading-30px rounded-lg px-4 py-2"> {/* Adjusted padding and height here */}
-              { loading ? <HashLoader size={35} color="#ffffff"/> : 'Sign Up'}
-            </button>
-          </div>
-           </form>
+        </div>
+        <div className="mt-3">
+          <button
+            disabled={loading}
+            type="submit"
+            className="w-full bg-primaryColor p-3 text-[16px] leading-7 rounded-md text-white ml-auto"
+          >
+            {loading ? <HashLoader size={25} color="#ffffff" /> : 'Update'}
+          </button>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
